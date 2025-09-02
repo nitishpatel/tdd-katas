c82a41395e925c118ce0a0f95387dfeb996b63c1
Nitish Patel <nitishp74@gmail.com>
2025-09-02 23:05:44 +0530

    refactor: is valid octet as a private method

--- ip-validator/ipvalidator.py
+++ ip-validator/ipvalidator.py
@@ -1,21 +1,19 @@
-def is_valid_octet(part):
-    if not part.isnumeric():
-        return False
-    if len(part) > 1 and part[0] == "0":
-        return False
-    part_value = int(part)
-    return 0 <= part_value <= 255
-
 class IPValidator:
-    def __init__(self):
-        pass
+
+    def _is_valid_octet(self,part):
+        if not part.isnumeric():
+            return False
+        if len(part) > 1 and part[0] == "0":
+            return False
+        part_value = int(part)
+        return 0 <= part_value <= 255
 
     def validate_ipv4_address(self,ip_address:str) -> bool:
         ip_parts = ip_address.split(".")
         if len(ip_parts) != 4:
             return False
 
-        if not all(is_valid_octet(part) for part in ip_parts):
+        if not all(self._is_valid_octet(part) for part in ip_parts):
             return False
 
         last_octet = int(ip_parts[3])
