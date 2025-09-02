06da2f8e921b87a2372e41033dfd8c19eccd1f54
Nitish Patel <nitishp74@gmail.com>
2025-09-02 22:36:32 +0530

    test: invalid non numeric parts

--- ip-validator/test_ipvalidator.py
+++ ip-validator/test_ipvalidator.py
@@ -17,3 +17,9 @@ def test_invalid_ip_address_ending_with_255(ip_validator):
 def test_invalid_no_of_parts(ip_validator):
     assert ip_validator.validate_ipv4_address("10.0.0") == False
     assert ip_validator.validate_ipv4_address("10.0.0.0.1") == False
+
+def test_invalid_non_numeric_parts(ip_validator):
+    assert ip_validator.validate_ipv4_address("10.0.0.a") == False
+    assert ip_validator.validate_ipv4_address("10.0..1") == False
+    assert ip_validator.validate_ipv4_address("10.0.-1.1") == False
+
