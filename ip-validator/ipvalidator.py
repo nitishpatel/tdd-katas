61004099cd12f2f9a8ff4492603daabf9f782229
Nitish Patel <nitishp74@gmail.com>
2025-09-02 23:02:40 +0530

    refactor: leading zeros check and last octet check

--- ip-validator/ipvalidator.py
+++ ip-validator/ipvalidator.py
@@ -1,16 +1,11 @@
-
-def check_leading_zeros(value):
-    if len(value) > 1 and value[0] == "0":
-        return True
-    return False
-
-def check_is_valid_part(part):
+def is_valid_octet(part):
     if not part.isnumeric():
         return False
+    if len(part) > 1 and part[0] == "0":
+        return False
     part_value = int(part)
-    if part_value >= 0 and part_value <= 255:
-        return True
-    return False
+    return 0 <= part_value <= 255
+
 class IPValidator:
     def __init__(self):
         pass
@@ -19,11 +14,11 @@ class IPValidator:
         ip_parts = ip_address.split(".")
         if len(ip_parts) != 4:
             return False
-        if ip_parts[3] == "0" or ip_parts[3] == "255":
-            return False
         for part in ip_parts:
-            if not check_is_valid_part(part):
+            if not is_valid_octet(part):
                 return False
-            if check_leading_zeros(part):
-                return False
+
+        last_octet = int(ip_parts[3])
+        if last_octet in (0,255):
+            return False
         return True
