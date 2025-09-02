b034d4aad715ce32f0d04e7e8959a17f95b86894
Nitish Patel <nitishp74@gmail.com>
2025-09-02 21:40:59 +0530

    test: added pytest fixtrue for ipvalidator class and added testcase for invalid ip address should return false

--- ip-validator/test_ipvalidator.py
+++ ip-validator/test_ipvalidator.py
@@ -1,5 +1,12 @@
+import pytest
 from ipvalidator import IPValidator
 
-def test_is_valid_ip_address():
-    ip_validator = IPValidator()
+@pytest.fixture
+def ip_validator():
+    return IPValidator()
+
+def test_is_valid_ip_address(ip_validator):
     assert ip_validator.validate_ipv4_address("1.1.1.1") == True
+
+def test_invalid_ip_addres(ip_validator):
+    assert ip_validator.validate_ipv4_address("0.0.0.0") == False
