add86ca3b964cc1b81c6835f15655879865e54c2
Nitish Patel <nitishp74@gmail.com>
2025-09-02 21:43:09 +0530

    feat: check for 0 or 255 in ending ip added

--- ip-validator/ipvalidator.py
+++ ip-validator/ipvalidator.py
@@ -5,4 +5,7 @@ class IPValidator:
         pass
 
     def validate_ipv4_address(self,ip_address:str) -> bool:
+        ip_parts = ip_address.split(".")
+        if ip_parts[3] == "0" or ip_parts[3] == "255":
+            return False
         return True
