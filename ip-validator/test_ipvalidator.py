d29f95f1758dac0edc7f397d632fa8bcb2692732
Nitish Patel <nitishp74@gmail.com>
2025-09-02 21:44:19 +0530

    test: invalid ip if ends with 255

--- ip-validator/test_ipvalidator.py
+++ ip-validator/test_ipvalidator.py
@@ -8,5 +8,8 @@ def ip_validator():
 def test_is_valid_ip_address(ip_validator):
     assert ip_validator.validate_ipv4_address("1.1.1.1") == True
 
-def test_invalid_ip_addres(ip_validator):
+def test_invalid_ip_address_ending_with_0(ip_validator):
     assert ip_validator.validate_ipv4_address("0.0.0.0") == False
+
+def test_invalid_ip_address_ending_with_255(ip_validator):
+    assert ip_validator.validate_ipv4_address("0.0.0.255") == False
