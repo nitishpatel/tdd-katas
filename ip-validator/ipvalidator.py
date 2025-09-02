1dfbf21a3ed2e73fee93b58f96a565e387934d11
Nitish Patel <nitishp74@gmail.com>
2025-09-02 22:32:58 +0530

    feat: invalid no of parts check added

--- ip-validator/ipvalidator.py
+++ ip-validator/ipvalidator.py
@@ -6,6 +6,8 @@ class IPValidator:
 
     def validate_ipv4_address(self,ip_address:str) -> bool:
         ip_parts = ip_address.split(".")
+        if len(ip_parts) != 4:
+            return False
         if ip_parts[3] == "0" or ip_parts[3] == "255":
             return False
         return True
